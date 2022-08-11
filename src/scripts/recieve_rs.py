2f7f8b5e3a70e6b450197bcfc8cb280a55e90a03
russapat <russapat.44@mail.kmutt.ac.th>
2022-08-11 17:32:43 +0700

    Can save images and Depth with controllable frequency

--- src/scripts/recieve_rs.py
+++ src/scripts/recieve_rs.py
@@ -1,5 +1,6 @@
 #! /usr/bin/python3
 
+from distutils.log import error
 from gc import callbacks
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
@@ -12,18 +13,18 @@ from message_filters import Subscriber
 import message_filters
 import os
 
-# imageQuantity = 0
+
 # starttime = 0
 class RecieveImage(object):
     def __init__(self):
         # Params
         self.image = None
         self.depth = None
+        
         self.br = CvBridge()
-        self.starttime = 0.0
-        self.imageQuantity = 0
         self.imagePath = '/home/russapat/obodroid_ws/src/rs_capture/src/image'
         self.depthPath = '/home/russapat/obodroid_ws/src/rs_capture/src/depth'
+        self.imageQuantity = 0
         # self.msg.header = Header()
         #Node cycle rate in Hz
         self.loop_rate = rospy.Rate(1)
@@ -31,27 +32,28 @@ class RecieveImage(object):
         self.depth_sub = Subscriber('/camera/aligned_depth_to_color/image_raw',Image)
         self.TimeSynchronizer = message_filters.ApproximateTimeSynchronizer([self.image_sub,self.depth_sub],queue_size=10,slop=0.5)
         self.TimeSynchronizer.registerCallback(self.callback)
-        # rospy.Subscriber("/camera/color/image_raw",Image,self.callback_rgb,queue_size=10)
-    #     rospy.Subscriber("/camera/aligned_depth_to_color/image_raw",Image,self.callback_depth,queue_size=10)
-    def callback(self, msg_img, msg_depth):
-        time_get_info = msg_img.header.stamp
-        rospy.loginfo('data recieve_{}')
-        self.image = msg_img
-        self.depth = msg_depth
-        time_get_info= msg_img.header.stamp 
-        msg_img = self.br.imgmsg_to_cv2(msg_img, desired_encoding="rgb8")
-        msg_depth = self.br.imgmsg_to_cv2(msg_depth, desired_encoding="passthrough")
-        if time_get_info - self.starttime >= 100000000.0:
-        #     cv2.imwrite(os.path.join(self.imagePath,"img_{}.jpg".format(time_get_info)),msg_img)
-        #     cv2.imwrite(os.path.join(self.depthPath,"depth_{}.png".format(time_get_info)),msg_depth)
-            rospy.loginfo('saveImages')
-        # cv2.imshow('rgb frame', rgb_image)
-        # cv2.waitKey(1)
-        # depthPath = '/home/russapat/obodroid_ws/src/rs_capture/src/depth'
-        # imagePath = '/home/russapat/obodroid_ws/src/rs_capture/src/image'
+        self.timer = rospy.Timer(rospy.Duration(1), self.timer_callback)
         
-        # cv2.imwrite(os.path.join(imagePath,"img_{}.jpg".format(times)),rgb_image)    #saveimage
-    
+   
+    def callback(self, msg_img, msg_depth):
+        # rospy.loginfo('data recieve')
+        self.image = self.br.imgmsg_to_cv2(msg_img, desired_encoding="bgr8")
+        self.depth = self.br.imgmsg_to_cv2(msg_depth, desired_encoding="passthrough")
+        
+    def timer_callback(self, get_timer):
+        rospy.loginfo('callback access')
+        get_timer = rospy.Time.now()
+        self.imageQuantity += 1
+        try:
+            if self.image.any() is not None and self.depth.any() is not None:
+                cv2.imwrite(os.path.join(self.imagePath,"img_{}.jpg".format(get_timer)),self.image)
+                rospy.loginfo('Save Images')
+                cv2.imwrite(os.path.join(self.depthPath,"depth_{}.png".format(get_timer)),self.depth)
+                rospy.loginfo('Save Depth Images')
+            # cv2.imshow(''rgb frame', rgb_image)
+        except:
+            print("waiting images")
+        # cv2.waitKey(1)
     
     def start(self):
         rospy.loginfo("Timing images")
@@ -63,7 +65,7 @@ class RecieveImage(object):
             self.loop_rate.sleep()
 
 if __name__ == '__main__':
-    rospy.init_node('recieveimage')
+    rospy.init_node('recieve_rs')
     my_node = RecieveImage()
     my_node.start()
 
